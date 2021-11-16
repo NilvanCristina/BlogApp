5c57ea56064d604a959444cd959e6fdc11e4e049
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-11-16 23:00:55 +0200

    Setup React app

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from "../../components/Footer/Footer";
+import './Home.css';
+
+function Home() {
+
+    return (
+        <div>
+            <p>Home</p>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
