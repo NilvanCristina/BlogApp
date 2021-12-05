4c8ca9b5bcedbe9d983dbba6dfe7af99ac7bf3b2
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-12-05 17:01:35 +0200

    Create Newsletter.js and Newsletter.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home/Home"
 import Contact from "./pages/Contact/Contact";
+import Newsletter from "./pages/Newsletter/Newsletter";
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/contact" element={ <Contact/> } />
+                <Route path="/newsletter" element={ <Newsletter/> } />
             </Routes>
         </Router>
   );
---
+++ src/Pages/Newsletter/Newsletter.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { createBrowserHistory } from 'history';
+import './Newsletter.css'
+import Footer from "../../components/Footer/Footer";
+
+
+function Newsletter() {
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    let history = createBrowserHistory();
+
+    function handleSubmit() {
+
+        if (email === '')
+            setErrorMessage("Unable to process empty fields");
+
+        if (email !== '') {
+            history.push('/');
+            history.go('/');
+        }
+    }
+
+    return (
+        <div>
+            <div className="Padding"/>
+
+            <div className="Form">
+                <h1>Subscribe to our newsletter!</h1>
+
+                <input type="email" placeholder="email@example.com" onChange={ event => setEmail(event.target.value) }/>
+                <div className="Padding"/>
+
+                { errorMessage && <div className="Error"><b>{ errorMessage }</b></div> }
+                <div className="Padding"/>
+
+                <button type="submit" className="Active" onClick = { handleSubmit }>Send</button>
+            </div>
+
+            <div className="Padding"/>
+
+            <Footer/>
+        </div>
+    )
+}
+
+export default Newsletter;
