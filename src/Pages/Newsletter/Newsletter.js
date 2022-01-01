53e240c5471dc602623ba1d24744ebbca0adc2ee
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2022-01-01 21:44:36 +0200

    Add background Newsletter

--- src/Pages/Newsletter/Newsletter.js
+++ src/Pages/Newsletter/Newsletter.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { createBrowserHistory } from 'history';
+import React, {useState} from "react"
+import {createBrowserHistory} from 'history';
 import './Newsletter.css'
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
@@ -9,6 +9,8 @@ import {asyncHandleNewsletter} from "../../utils/ApiCalls";
 function Newsletter() {
     const [user_email, setUserEmail] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+
+    const wallpaperImage = 'url("https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")';
 
     let history = createBrowserHistory();
 
@@ -29,23 +31,29 @@ function Newsletter() {
 
     return (
         <div>
-            <Navbar />
+            <Navbar/>
 
-            <div className="Padding"/>
-
-            <div className="Form">
-                <h1>Subscribe to our newsletter!</h1>
-
-                <input type="email" placeholder="email@example.com" onChange={ event => setUserEmail(event.target.value) }/>
+            <div className="Page" style={{backgroundImage: `${wallpaperImage}`}}>
+                <div className="Padding"/>
                 <div className="Padding"/>
 
-                { errorMessage && <div className="Error"><b>{ errorMessage }</b></div> }
-                <div className="Padding"/>
+                <div className="Form">
+                    <h1>Subscribe to our newsletter!</h1>
 
-                <button type="submit" className="Active" onClick = { handleNewsletter }>Send</button>
+                    <input type="email" placeholder="email@example.com"
+                           onChange={event => setUserEmail(event.target.value)}/>
+                    <div className="Padding"/>
+
+                    {errorMessage && <div className="Error"><b>{errorMessage}</b></div>}
+                    <div className="Padding"/>
+
+                    <button type="submit" className="Active" onClick={handleNewsletter}>Send</button>
+                </div>
+
+                <div className="Padding"/>
+                <div className="Padding"/>
+                <div className="Padding"/>
             </div>
-
-            <div className="Padding"/>
 
             <Footer/>
         </div>
