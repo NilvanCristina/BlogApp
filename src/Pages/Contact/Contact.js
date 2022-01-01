761725a89f454076d43921926990c5588c3aca38
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2022-01-01 21:41:01 +0200

    Add background for Contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { createBrowserHistory } from 'history';
+import React, {useState} from "react"
+import {createBrowserHistory} from 'history';
 import './Contact.css'
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
@@ -12,6 +12,8 @@ function Contact() {
     const [message, setMessage] = useState("");
 
     const [errorMessage, setErrorMessage] = useState("");
+
+    const wallpaperImage = 'url("https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")';
 
     let history = createBrowserHistory();
 
@@ -32,32 +34,35 @@ function Contact() {
 
     return (
         <div>
-            <Navbar />
+            <Navbar/>
 
-            <div className="Padding"/>
-
-            <div className="Form">
-                <h1>Contact us!</h1>
-
-                <input type="text" placeholder="First name" onChange={ event => setFirstName(event.target.value) }/>
+            <div className="Page" style={{backgroundImage: `${wallpaperImage}`}}>
                 <div className="Padding"/>
 
-                <input type="text" placeholder="Last name" onChange={ event => setLastName(event.target.value) }/>
-                <div className="Padding"/>
+                <div className="Form">
+                    <h1>Contact us!</h1>
 
-                <input type="email" placeholder="email@example.com" onChange={ event => setEmail(event.target.value) }/>
-                <div className="Padding"/>
+                    <input type="text" placeholder="First name" onChange={event => setFirstName(event.target.value)}/>
+                    <div className="Padding"/>
 
-                <textarea rows="4" placeholder="Message" onChange = { event => setMessage(event.target.value) }/>
-                <div className="Padding"/>
+                    <input type="text" placeholder="Last name" onChange={event => setLastName(event.target.value)}/>
+                    <div className="Padding"/>
 
-                { errorMessage && <div className="Error"><b>{ errorMessage }</b></div> }
-                <div className="Padding"/>
+                    <input type="email" placeholder="email@example.com"
+                           onChange={event => setEmail(event.target.value)}/>
+                    <div className="Padding"/>
 
-                <button type="submit" className="Active" onClick = { handleContact }>Send</button>
+                    <textarea rows="4" placeholder="Message" onChange={event => setMessage(event.target.value)}/>
+                    <div className="Padding"/>
+
+                    {errorMessage && <div className="Error"><b>{errorMessage}</b></div>}
+                    <div className="Padding"/>
+
+                    <button type="submit" className="Active" onClick={handleContact}>Send</button>
+                </div>
+
+                <div className="Padding"/>
             </div>
-
-            <div className="Padding"/>
 
             <Footer/>
         </div>
